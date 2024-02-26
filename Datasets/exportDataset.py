38a3fb67cf1a1b60e55f2c4f9a60ceb1038f31e1
Anthony Z <anthony.hp.zheng@gmail.com>
2024-02-26 17:51:52 -0600

    Add files via upload

---
+++ Datasets/exportDataset.py
@@ -0,0 +1,20 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+dataset = foz.load_zoo_dataset(
+    "open-images-v6",
+    split="validation",
+    max_samples=1000,
+    label_types=["detections"],
+    classes="Human head",
+    only_matching=True,
+)
+
+dataset.export(
+    export_dir="/Users/anthonyzheng/Desktop/datasets",
+    dataset_type=fo.types.COCODetectionDataset,
+    label_field="ground_truth",
+)
+
+session = fo.launch_app(dataset)
+session.wait()
