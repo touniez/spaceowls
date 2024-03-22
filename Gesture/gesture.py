18e6bd27340c3c02734ecd64ed57c316bc246dcb
Anthony Z <anthony.hp.zheng@gmail.com>
2024-03-22 16:09:49 -0500

    Update gesture.py

--- Gesture/gesture.py
+++ Gesture/gesture.py
@@ -11,31 +11,46 @@ import numpy as np
 from picamera2 import Picamera2
 
 # STEP 2: Create an GestureRecognizer object.
-base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
-options = vision.GestureRecognizerOptions(base_options=base_options)
+base_options = python.BaseOptions(model_asset_path='/home/pi/Downloads/gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options,
+                                          running_mode=vision.RunningMode.IMAGE,
+                                          num_hands=2,
+                                          min_hand_detection_confidence=0.4,
+                                          min_hand_presence_confidence=0.4,
+                                          min_tracking_confidence=0.4)
+# options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
 
 picam2 = Picamera2()
 x_dim = 2304
 y_dim = 1296
+
+DESIRED_HEIGHT = 224
+DESIRED_WIDTH = 224
+
 picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (x_dim, y_dim)}))
+# picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (DESIRED_WIDTH, DESIRED_HEIGHT)}))
 
 picam2.start()
 disp_width = int(2304 / 4)
 disp_height = int(1296 / 4)
 
+
 while True:
     # STEP 3: Load the input image.
     original_img = picam2.capture_array()
     img_display = cv2.resize(original_img, (disp_width, disp_height))
-    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=original_img)
+    resized_img = cv2.resize(original_img, (DESIRED_WIDTH, DESIRED_HEIGHT))
+    rgb_image = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
 
     # STEP 4: Recognize gestures in the input image.
     recognition_result = recognizer.recognize(image)
 
     # STEP 5: Process the result. In this case, visualize it.
-    top_gesture = recognition_result.gestures[0][0]
-    print(top_gesture)
+    for i, gesture in enumerate(recognition_result.gestures):
+        # Get the top gesture from the recognition result
+        print("Top Gesture Result: ", gesture[0].category_name)
 
     cv2.imshow('Gesture detector', img_display)
     cv2.waitKey(1)
